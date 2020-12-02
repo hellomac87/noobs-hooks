c543d8c452035b15961ee8a4dace3ca2897d5182
hellomac87 <hellomac87@gmail.com>
2020-12-02 23:06:38 +0900

    useClick

--- hooks.js
+++ hooks.js
@@ -35,3 +35,20 @@ export const useTitle = (initialTitle) => {
   useEffect(updateTitle, [title]);
   return setTitle;
 };
+
+const useClick = (onClick) => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+  if (typeof onClick !== "function") return;
+  return element;
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+  if (typeof onClick !== "function") return;
+  return element;
 };
 
 function App() {
-  const titleUpdater = useTitle("Loading....");
-  setTimeout(() => {
-    titleUpdater("Dobby");
-  }, 5000);
-  // useEffect(() => {
+  const sayHello = () => console.log("Hello");
+  const title = useClick(sayHello);
 
-  // }, []);
   return (
     <div className="App">
-      <div>hi</div>
+      <h1 ref={title}>hi</h1>
     </div>
   );
 }
