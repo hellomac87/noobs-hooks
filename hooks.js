3c46c37d37f4ec80449ad9fa269637791ab20999
hellomac87 <hellomac87@gmail.com>
2020-12-03 23:13:06 +0900

    useConfirm

--- hooks.js
+++ hooks.js
@@ -52,3 +52,18 @@ const useClick = (onClick) => {
   if (typeof onClick !== "function") return;
   return element;
 };
+
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") return;
+  if (!onCancel || typeof onCancel !== "function") return;
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
+    }
+  };
+
+  return confirmAction;
+};
--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import { useEffect, useState, useRef } from "react";
 
-const useClick = (onClick) => {
-  const element = useRef();
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") return;
+  if (!onCancel || typeof onCancel !== "function") return;
 
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
     }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, [onClick]);
-  if (typeof onClick !== "function") return;
-  return element;
+  };
+
+  return confirmAction;
 };
 
 function App() {
-  const sayHello = () => console.log("Hello");
-  const title = useClick(sayHello);
+  const deleteWorld = () => console.log("deleting the world");
+  const abort = () => console.log("abort");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
 
   return (
     <div className="App">
-      <h1 ref={title}>hi</h1>
+      <h1>hi</h1>
+      <button onClick={confirmDelete}>Delete world</button>
     </div>
   );
 }
