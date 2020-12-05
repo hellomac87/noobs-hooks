ace66f3121947b3dde326f06226f06db4fea9ea3
hellomac87 <hellomac87@gmail.com>
2020-12-06 00:14:26 +0900

    useFullScreen

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import { useEffect, useState, useRef, useCallback } from "react";
 
-const useScroll = () => {
-  const [state, setState] = useState({
-    x: 0,
-    y: 0,
-  });
+const useFullScreen = (callback) => {
+  const element = useRef();
 
-  const onScroll = () => {
-    setState({
-      x: window.scrollX,
-      y: window.scrollY,
-    });
+  const runCallback = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
   };
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, []);
-  return state;
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      }
+      runCallback(true);
+    }
+  };
+
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCallback(false);
+  };
+  return { element, triggerFull, exitFull };
 };
 
 function App() {
-  const { y } = useScroll();
+  const onFullScreen = (isFull) => {
+    console.log(isFull);
+  };
+  const { element, triggerFull, exitFull } = useFullScreen(onFullScreen);
+
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y < 100 ? "red" : "blue" }}>
-        HELL
-      </h1>
+      <div ref={element}>
+        <img
+          src="https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2FBoiledMovie%2Ff5bf6f8afe9a440bab072c4cb96d5850.jpg"
+          alt="full screen"
+        />
+        <button onClick={exitFull}>exitFull</button>
+      </div>
+
+      <button onClick={triggerFull}>Make fillscreen</button>
     </div>
   );
 }
