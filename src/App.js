81278e9bb0b446982bd4f11a705e53577c9fefe0
hellomac87 <hellomac87@gmail.com>
2020-12-06 22:51:01 +0900

    useAxios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import { useEffect, useState, useRef, useCallback } from "react";
-
-const useNotification = (title, options) => {
-  if (!("Notification" in window)) {
-    return;
-  }
-  const fireNotification = () => {
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          new Notification(title, options);
-        } else {
-          return;
-        }
-      });
-    } else {
-      new Notification(title, options);
-    }
-  };
-  return fireNotification;
-};
+import useAxios from "./useAxios";
 
 function App() {
-  const fireNotification = useNotification("Can i steal your heart?", {
-    body: "i love you",
+  const { loading, error, data, refetch } = useAxios({
+    url:
+      "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json",
   });
 
+  console.log(`data : ${JSON.stringify(data)}`);
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <button onClick={fireNotification}>button</button>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>refetch</button>
     </div>
   );
 }
